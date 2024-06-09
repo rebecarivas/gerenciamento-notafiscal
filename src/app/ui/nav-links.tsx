6d6ed340464e9d57cd0067719ac51b035df46d89
rebecarivas <rebecarivas@gmail.com>
2024-06-09 07:32:05 -0300

    feat: troca a tag <a> pelo componente <Link> do Next para navegação entre as páginas

--- src/app/ui/nav-links.tsx
+++ src/app/ui/nav-links.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import {
   UserGroupIcon,
   HomeIcon,
@@ -20,14 +21,14 @@ export default function Navlinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
             className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
